872c26497b45fbeb9f20c3b69a3ef016bc440391
Davut Kember <davutkmbr@gmail.com>
2024-06-13 20:48:44 +0300

    update welcome screen styling

--- src/screens/welcome-screen.tsx
+++ src/screens/welcome-screen.tsx
@@ -1,15 +1,11 @@
 import { observer } from "mobx-react-lite";
-import { useEffect, useState } from "react";
 
-import { LoadingWrapper } from "@/components/loading-wrapper";
 import {
   Screen,
   ScreenContent,
   ScreenHeader,
   ScreenTitle,
 } from "@/components/ui/screen";
-import { useStore } from "@/contexts/store-context";
-import { cn } from "@/lib/utils";
 
 export const WelcomeScreen = observer(() => {
   return (
@@ -18,7 +14,7 @@ export const WelcomeScreen = observer(() => {
         <ScreenTitle>Welcome Screen</ScreenTitle>
       </ScreenHeader>
       <ScreenContent>
-        <div>Welcome to Grispi Plugin React Starter.</div>
+        <div className="p-6">Welcome to Grispi Plugin React Starter.</div>
       </ScreenContent>
     </Screen>
   );
