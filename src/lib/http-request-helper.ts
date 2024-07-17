f43c08668d9a6d8c55765e027a9afeae8d71bab3
timur <timurturbill@gmail.com>
2024-07-17 23:44:09 +0300

    remove proxy endpoint while send request

--- src/lib/http-request-helper.ts
+++ src/lib/http-request-helper.ts
@@ -1,6 +1,6 @@
 export class HttpRequestHelper {
-    proxyUrl: string = "http://localhost:8080";
-    baseUrl: string = "https://stageapi.trendyol.com/stagesapigw";
+    proxyUrl: string = ""; //"http://localhost:8080/";
+    baseUrl: string = this.proxyUrl + "https://stageapi.trendyol.com/stagesapigw";
     username: string = "3xATCK5OkR6p8LBohvse";
     password: string = "QqfFXiUat1x82D4vdSbS";
     headers: Record<string, string> = {
@@ -9,7 +9,7 @@ export class HttpRequestHelper {
     };
 
     async send<T>(url: string, options: RequestInit): Promise<T | null> {
-        const response = await fetch(`${this.proxyUrl}/${this.baseUrl}/${url}`, {
+        const response = await fetch(`${this.baseUrl}/${url}`, {
             ...options,
             headers: { ...this.headers, ...options.headers },
         });
