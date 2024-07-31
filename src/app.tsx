47758dfedb8a6606a91f65e4f8e04b464de0f6f8
timur <timurturbill@gmail.com>
2024-07-31 14:41:19 +0300

    use grispi proxy

--- src/app.tsx
+++ src/app.tsx
@@ -2,16 +2,14 @@
 import { TrendyolProvider } from "./contexts/trendyol-context";
 import { StoreProvider } from "./contexts/store-context";
 import { WelcomeScreen } from "./screens/welcome-screen";
-import { GrispiProvider } from "./contexts/grispi-context";
+//import { GrispiProvider } from "./contexts/grispi-context";
 
 const App = () => {
   return (
     <StoreProvider>
-      <GrispiProvider>
         <TrendyolProvider>
           <WelcomeScreen />
         </TrendyolProvider>
-      </GrispiProvider>
     </StoreProvider>
   );
 };
