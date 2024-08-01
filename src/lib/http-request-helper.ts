07c3945413d3ea0fe591dfcfdc347763115994be
timur <timurturbill@gmail.com>
2024-08-01 14:19:32 +0300

    fix

--- src/lib/http-request-helper.ts
+++ src/lib/http-request-helper.ts
@@ -3,13 +3,16 @@ export class HttpRequestHelper {
     //proxyUrl: string = "http://localhost:8080/";
     baseUrl: string = this.proxyUrl + "https://api.trendyol.com/sapigw";
     //baseUrl: string = "https://api.trendyol.com/sapigw";
-    username: string = "A8ahxcWDsmoay2OJJtrN";
-    password: string = "PjqJAsoEPYRJ0g5hh9rs";
+    //username: string = "997502";
+    //password: string = "A8ahxcWDsmoay2OJJtrN";
+    //trendyolAuthorizationString: string = `Basic ${btoa(`${this.username}:${this.password}`)}`;
     headers: Record<string, string> = {
         "Content-Type": "application/json",
-        "Authorization": `Basic ${btoa(`${this.username}:${this.password}`)}`
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZWRlbGVyIiwic3ViIjoiYWJvbmVAZG9ydWtkZXN0YW4uY29tIiwieGlkIjoxLCJkZXYiOmZhbHNlLCJyb2xlcyI6WyJST0xFX0FETUlOIl0sImlzcyI6ImdyaXNwaS1hcGk6MjA0MzY4MTMzMiIsImV4cCI6MTcyMjUxNDcyMCwiaWF0IjoxNzIyNTExMTIwLCJqdGkiOiIwNWMzMjc3NS1lNDEzLTQzNWQtODRjYy00NGM2YTE3ZjIzMGYifQ.BfD2F-geUV5b1XLmr-jOW9_VWgII4ZUoF1bvSeUtu7M",
+        "Trendyol-Authorization": "Basic QThhaHhjV0RzbW9heTJPSkp0ck46UGpxSkFzb0VQWVJKMGc1aGg5cnM=",
+        "tenantId": "dedeler"
     };
-
+    
     async send<T>(url: string, options: RequestInit): Promise<T | null> {
         const response = await fetch(`${this.baseUrl}/${url}`, {
             ...options,
